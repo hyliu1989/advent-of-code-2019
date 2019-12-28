69788034e72a3415eb969d4523d7533c4b0d0f1d
Hsiou-Yuan Liu <hyhliu1989@gmail.com>
2019-12-28 14:01:41 -0500

    use numpy boolean array to record reachable_keys, blockers and collected_keys

--- day18.py
+++ day18.py
@@ -16,9 +16,9 @@ class SearchTrace:
             cls.min_step = value
 
     def __init__(self, to_init_search=False):
-        self.reachable_keys = set()
-        self.blocked_lock_and_segments = []
-        self.collected_keys = set()
+        self.reachable_keys = np.zeros(26+1, np.bool)
+        self.blockers =  np.zeros(26*2+1, np.bool)
+        self.collected_keys = np.zeros(26+1, np.bool)
         self.step = 0
         self.head = (40,40)
         self.trace = []
@@ -38,7 +38,7 @@ class SearchTrace:
     def copy(self):
         ret = SearchTrace()
         ret.reachable_keys = self.reachable_keys.copy()
-        ret.blocked_lock_and_segments = self.blocked_lock_and_segments.copy()
+        ret.blockers = self.blockers.copy()
         ret.collected_keys = self.collected_keys.copy()
         ret.step = self.step
         ret.head = self.head
@@ -50,11 +50,10 @@ class SearchTrace:
         # for quad in [maputil.quadrants[1][1:2]]:
             for seg in quad:
                 search_keys_in_seg_and_children_until_blocked(
-                    seg, self.reachable_keys, self.blocked_lock_and_segments,
-                    self.collected_keys)
+                    seg, self.reachable_keys, self.blockers, self.collected_keys)
 
     def get_key(self, key):
-        assert key in self.reachable_keys
+        assert self.reachable_keys[key] == True
 
         # count the step to move to where the key is
         key_pos = maputil.item_positions[key]
@@ -63,46 +62,53 @@ class SearchTrace:
         self.step += n_steps
         self.head = key_pos
 
-        self.reachable_keys.remove(key)
-        self.collected_keys.add(key)
+        self.reachable_keys[key] = False
+        self.collected_keys[key] = True
         self.trace.append(key)
+        obtained_key = key
 
         if self.step > self.get_min_step():
             return 'abort'
 
-        original = self.blocked_lock_and_segments.copy()
-        for item, seg in original:
-            if abs(item) == key:  # check for both key and door
-                # the locked door is unlocked!
-                self.blocked_lock_and_segments.remove((item, seg))
+        # Check the removed blockers
+        for item in range(-26,27):
+            if not self.blockers[item] or item == 0:
+                continue
+
+            if abs(item) == obtained_key:  # this checks for both key and door
+                # Remove the blocker from the list
+                self.blockers[item] = False
+                # Recursively search
+                blocker_pos = maputil.item_positions[item]
+                seg = maputil.segment_map[blocker_pos]
                 search_keys_in_seg_and_children_until_blocked(
-                    seg, self.reachable_keys, self.blocked_lock_and_segments,
-                    self.collected_keys)
+                    seg, self.reachable_keys, self.blockers, self.collected_keys
+                )
 
 
-def search_keys_in_seg_and_children_until_blocked(seg, reachable_keys, blocked_lock_and_segments,
-                                                  collected_keys):
+def search_keys_in_seg_and_children_until_blocked(seg, reachable_keys, blockers, collected_keys):
     for item, _ in seg.ordered_items:
         stopped = False
         if 1 <= item <= 26:  # key
-            if item in collected_keys:
+            if collected_keys[item]:
                 pass
             else:
-                reachable_keys.add(item)
+                reachable_keys[item] = True
                 stopped = True
         elif -26 <= item <= -1:
-            if -item in collected_keys:
+            if collected_keys[-item]:
                 pass
             else:
                 # blocked by the door
                 stopped = True
+        
+        # return if stopped
         if stopped:
-            blocked_lock_and_segments.append((item, seg))
+            blockers[item] = True
             return
 
     for s in seg.children:
-        search_keys_in_seg_and_children_until_blocked(s, reachable_keys, blocked_lock_and_segments,
-                                                      collected_keys)
+        search_keys_in_seg_and_children_until_blocked(s, reachable_keys, blockers, collected_keys)
 
 
 if __name__ == '__main__':
@@ -112,14 +118,16 @@ if __name__ == '__main__':
     def dfs(search_trace):
         # record only the trace that requires the minimal step
         min_step = SearchTrace.get_min_step()
-        if len(search_trace.reachable_keys) == 0:
+        if np.sum(search_trace.reachable_keys) == 0:
             if min_step > search_trace.step:
                 SearchTrace.set_min_step(search_trace.step)
                 print(search_trace.step)
                 trace_list.append(search_trace)
             return
 
-        for k in search_trace.reachable_keys:
+        for k in range(1,27):
+            if not search_trace.reachable_keys[k]:
+                continue
             s = search_trace.copy()
             info = s.get_key(k)
             if info != 'abort':
@@ -130,21 +138,23 @@ if __name__ == '__main__':
     def test():
         for i in [1, 4, 5, 9, 8, 10, 17, 16, 22, 23, 7, 24, 26, 11, 12, 13]:
             start_trace.get_key(i)
-            print('blocked', [door for door, _ in start_trace.blocked_lock_and_segments])
+            print('blocked', [door for door, _ in start_trace.blockers])
             print('reachable_keys', start_trace.reachable_keys)
 
 
     def _bfs(search_trace, task_list):
         # record only the trace that requires the minimal step
         min_step = SearchTrace.get_min_step()
-        if len(search_trace.reachable_keys) == 0:
+        if np.sum(search_trace.reachable_keys) == 0:
             if min_step > search_trace.step:
                 SearchTrace.set_min_step(search_trace.step)
                 print('min step', search_trace.step)
                 trace_list.append(search_trace)
             return
 
-        for k in search_trace.reachable_keys:
+        for k in range(1,27):
+            if not search_trace.reachable_keys[k]:
+                continue
             s = search_trace.copy()
             info = s.get_key(k)
             if info != 'abort':
